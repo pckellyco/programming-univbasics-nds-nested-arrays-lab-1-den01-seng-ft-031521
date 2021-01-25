f67ac6057a12045c94a8728cc54339d31c578e54
Paige Kelly <pckelly.co@gmail.com>
2021-01-25 23:31:52 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,6 +35,9 @@ end
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
   
+matrix = [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
+new_value = "Lemons"
+  
 def matrix_update(matrix, row, column, new_value)
   matrix[column][row] = new_value
   matrix
